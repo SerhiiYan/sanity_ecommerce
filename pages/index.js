ef3e02cbda87caf54c573f405c4e0550d8af2643
YanchevskyiSerhii <serhii.yanchevskii@gmail.com>
2022-07-28 17:36:52 +0300

    feat: add context logic, cart

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
-const index = () => {
-  return <div></div>;
+const Home = ({ products, bannerData }) => {
+  return (
+    <>
+      {console.log(products)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    </>
+  );
 };
 
-export default index;
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;
